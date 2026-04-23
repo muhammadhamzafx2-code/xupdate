a8a156af0584a902f918f5e001f5251df291e76b
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-04-23 21:41:21 +0000

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,6 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-com
 
 firebase.initializeApp({
   apiKey: "AIzaSyD6vGQwB7nHtZFvCUls9TBuscXcP1ER77M",
-  authDomain: "mvhdhamxa-792bf.firebaseapp.com",
   projectId: "mvhdhamxa-792bf",
   messagingSenderId: "214112128362",
   appId: "1:214112128362:web:8892c9bf90d42c7fd22913"
@@ -11,21 +10,13 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// background notification
-messaging.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage(() => {
   self.registration.showNotification("Reminder", {
     body: "Visit google.com",
-    icon: "https://www.google.com/favicon.ico",
-    data: {
-      url: "https://www.google.com"
-    }
+    data: { url: "https://google.com" }
   });
 });
 
-// 🔥 CLICK ACTION
-self.addEventListener("notificationclick", function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
-  );
+self.addEventListener("notificationclick", e => {
+  e.waitUntil(clients.openWindow("https://google.com"));
 });
