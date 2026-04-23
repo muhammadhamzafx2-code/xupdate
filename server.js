f336099e092e5284b195bfe7acd0c80f45a51493
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-04-23 01:13:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.json());
 
 // 🔑 Load Firebase key from environment variable (Render)
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+// fix newline issue
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
